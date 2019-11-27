06d4bef9cd392084571a6b09aa26ca9c838ef198
betocostadev <robertomarx@gmail.com>
2019-11-27 16:04:16 -0300

    added new factorial version and tested performance

--- hacker-rank/02-func-to-fac.js
+++ hacker-rank/02-func-to-fac.js
@@ -9,8 +9,19 @@ Explanation: 4! = 4 x 3 x 2 x 1 = 24
  */
 
 /* My attempt: */
+// Alta performance
+function fatorial(numero) {
+  let resultado = 1;
+  // Ou apenas inicie o i = 2, nem precisa do if.
+  if (numero < 2) return resultado;
+  for (let i = 1; i <= numero; i++) {
+    resultado *= i;
+  }
+  return resultado;
+}
+console.log('V1 - for loop: ', fatorial(5))
 
-
+// Versão mais lenta devido a colocar dentro de arrays e por fim rodar o reduce.
 function factorial(n) {
     let numbers = [n];
     while (n > 1) {
@@ -25,6 +36,7 @@ console.log(factorial(5));
 
 
 // Function below based on Eloquent JavaScript Book, chapter 1.
+// Apesar de ser recursiva, a performance ainda é melhor que a versão acima.
 function factorialElo (num) {
     if (num == 0) {
         return 1;
